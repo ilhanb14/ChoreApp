80bc4df8d299665ca2dfd645772ccaadc4d83b68
Merlinruy <ruysen.merlijn@gmail.com>
2025-06-03 14:37:55 +0200

    added component to create chores

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Livewire\CreateChore;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/create-chore', CreateChore::class)
+    ->name('create-chore');
