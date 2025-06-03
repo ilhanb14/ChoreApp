324e82979078939c89e80b26d75c82cc978a4896
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-06-03 11:56:37 +0200

    Replaced self_assigned with assigned_by

---
+++ database/migrations/2025_06_03_094825_replace_self_assigned_with_assigned_by_on_task_user_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('task_user', function (Blueprint $table) {
+            $table->dropColumn('self_assigned');
+            $table->bigInteger('assigned_by');
+            $table->foreign('assigned_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('task_user', function (Blueprint $table) {
+            $table->boolean('self_assigned');
+            $table->dropColumn('assigned_by');
+        });
+    }
+};
