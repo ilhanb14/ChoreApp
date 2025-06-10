31520a19356bb9d8d8b43973cf7f6d8b4bdf9e04
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-06-10 13:24:43 +0200

    Routes in auth group

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,7 @@ Route::get('/', function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('/create-chore', CreateChore::class)
-    ->name('create-chore');
-Route::get('/chores', ChoreList::class)
-    ->name('chores');    
-Route::get('/chores/edit/{chore}', EditChore::class)
-    ->name('edit-chore');
-
-// Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth'])->group(function () {
     // Family routes
     Route::post('/families', [FamilyController::class, 'create'])->name('families.create');
     Route::post('/families/{family}/invites', [FamilyController::class, 'sendInvite'])->name('families.invites.send');
@@ -32,14 +25,23 @@ Route::get('/chores/edit/{chore}', EditChore::class)
     Route::get('/invites', [InviteController::class, 'index'])->name('invites.index');
     Route::post('/invites/{invite}/accept', [InviteController::class, 'accept'])->name('invite.accept');
     Route::post('/invites/{invite}/decline', [InviteController::class, 'decline'])->name('invite.decline');
-// });
- 
 
-Route::get('/rewards', [RewardsController::class, 'getRewardsView'])
-    ->name('rewards');
-Route::post('/claim-reward', [RewardsController::class, 'claimReward'])
-    ->name('claim-reward');
-Route::post('/remove-reward', [RewardsController::class, 'removeReward'])
-    ->name('remove-reward');
+    // Chore routes
+    Route::get('/create-chore', CreateChore::class)
+        ->name('create-chore');
+    Route::get('/chores', ChoreList::class)
+        ->name('chores');    
+    Route::get('/chores/edit/{chore}', EditChore::class)
+        ->name('edit-chore');
+
+    // Reward routes
+    Route::get('/rewards', [RewardsController::class, 'getRewardsView'])
+        ->name('rewards');
+    Route::post('/claim-reward', [RewardsController::class, 'claimReward'])
+        ->name('claim-reward');
+    Route::post('/remove-reward', [RewardsController::class, 'removeReward'])
+        ->name('remove-reward');
+});
+
 
 
