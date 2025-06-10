63d022cd553c9e87a315c2ab02f6f77622527f18
Karel Segers <karelsegers@outlook.com>
2025-06-10 11:54:48 +0200

    Stupid small mistakes from merging

--- app/Models/Family.php
+++ app/Models/Family.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use app\Models\User;
+use App\Models\User;
 
 class Family extends Model
 {
