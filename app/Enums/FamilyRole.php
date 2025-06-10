3f817b839ba2d1f20a248546aef1ddad86484ca1
Karel Segers <karelsegers@outlook.com>
2025-06-10 11:34:38 +0200

    Added labels to the Enums to be able to change the value that is displayed

--- app/Enums/FamilyRole.php
+++ app/Enums/FamilyRole.php
@@ -6,4 +6,20 @@ enum FamilyRole: string
 {
     case Adult = 'adult';
     case Child = 'child';
+
+    public function label(): string
+    {
+        return match($this) {
+            self::Adult => 'adult',     // Display as "adult" in UI
+            self::Child => 'child',     // Display as "child" in UI
+        };
+    }
+    
+    public static function options(): array
+    {
+        return [
+            self::Adult->value => self::Adult->label(),
+            self::Child->value => self::Child->label(),
+        ];
+    }
 }
