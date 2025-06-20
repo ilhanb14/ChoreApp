8d5144fcae2deb3de1380e208d11da750730d6c0
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-06-20 14:05:35 +0200

    Fixed plural chores labels

--- app/Filament/Resources/ChoreResource/Pages/CreateChore.php
+++ app/Filament/Resources/ChoreResource/Pages/CreateChore.php
@@ -9,4 +9,6 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateChore extends CreateRecord
 {
     protected static string $resource = ChoreResource::class;
+
+    protected static ?string $title = 'Create Chore';
 }
