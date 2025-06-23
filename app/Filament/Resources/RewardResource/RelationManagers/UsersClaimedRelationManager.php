e50128a62cb347a5bc7dd624635b7158b673669f
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-06-23 11:44:26 +0200

    Maually added filament resources from dev

---
+++ app/Filament/Resources/RewardResource/RelationManagers/UsersClaimedRelationManager.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Resources\RewardResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UsersClaimedRelationManager extends RelationManager
+{
+    protected static string $relationship = 'usersClaimed';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label('User Name')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->label('Email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('pivot.created_at')
+                    ->label('Claimed')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\AttachAction::make()
+                    ->form(fn (Tables\Actions\AttachAction $action): array => [
+                        $action->getRecordSelect()
+                            ->searchable()
+                            ->preload()
+                            ->options(function () {
+                                $reward = $this->getOwnerRecord();
+                                return $reward->family->members->pluck('name', 'id');   // Get all users in this rewards family
+                            }),
+                    ]),
+            ])
+            ->actions([
+                Tables\Actions\DetachAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DetachBulkAction::make(),
+                ]),
+            ]);
+    }
+}
